28b0207962747d58600b443eed2a42268e0ec1e7
Jeeweon Jung <jeewon.leo.jung@gmail.com>
2024-02-21 22:24:21 -0500

    add: setup files

---
+++ setup.py
@@ -0,0 +1,14 @@
+from distutils.core import setup
+
+setup(
+    name = 'a_dcf',
+    packages = ['my_python_package'],
+    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    description = 'a-dcf preliminary release',
+    author = 'Jee-weon Jung',
+    author_email = 'jeeweonj@ieee.org',
+    url = 'https://github.com/shimhz/a_dcf',
+    download_url = 'download link you saved',
+    keywords = ['adcf', 'a-dcf', 'a_dcf'],
+    classifiers = [],
+)
