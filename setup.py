2687efa0a8ce8144e2d6ae3229efbf4f211e779e
Jeeweon Jung <jeewon.leo.jung@gmail.com>
2024-02-22 00:08:28 -0500

    update: version and package

--- setup.py
+++ setup.py
@@ -3,8 +3,8 @@ from setuptools import find_packages
 
 setup(
     name = 'a_dcf',
-    packages = find_packages(include=["dcf"]),
-    version = '0.0.1',  # Ideally should be same as your GitHub release tag varsion
+    packages = find_packages(include=["*dcf"]),
+    version = '0.0.4',  # Ideally should be same as your GitHub release tag varsion
     description = 'a-dcf preliminary release',
     author = 'Jee-weon Jung',
     author_email = 'jeeweonj@ieee.org',
